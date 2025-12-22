85348d9bd571a94a277a8fa4cd5e059db41daebb
checa2000 <checa200017@gmail.com>
2025-12-22 20:59:10 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "gastos-app-v1";
+const FILES = [
+  "gastos.html",
+  "manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
